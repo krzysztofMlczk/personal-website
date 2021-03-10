6931ad478ef2c0380282363f9dfd846920ed84a9
Krzysztof Małczak <gajderstudio@gmial.com>
2021-03-10 18:44:19 +0100

    Fixed navbar issue. Finished Home Page

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,19 +1,14 @@
 const openNav = () => {
   const burgerMenu = document.querySelector(".burger");
-  const closeMenu = document.querySelector(".close");
   const navLinks = document.querySelector(".nav-links");
   let opened = false;
 
   const toggleNav = () => {
     opened = !opened;
     navLinks.classList.toggle("opened");
-
-    burgerMenu.classList.toggle("off");
-    closeMenu.classList.toggle("off");
   };
 
   burgerMenu.addEventListener("click", toggleNav);
-  closeMenu.addEventListener("click", toggleNav);
   navLinks.addEventListener("click", toggleNav);
   window.addEventListener("resize", () => {
     if (opened && document.documentElement.clientWidth >= 992) toggleNav();
