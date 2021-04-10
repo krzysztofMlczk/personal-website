a7e07b92a04d71dc4ae20ee332cc48ecf37c8bb7
Krzysztof Małczak <gajderstudio@gmial.com>
2021-04-10 16:49:36 +0200

    Navbar generating script + noscript navbar alternative

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,59 @@
-const openNav = () => {
+// Turn off js:
+// type in URL: about:config
+// look up javascript.enabled
+const generateNav = () => {
+  const links = [
+    { href: "#about", content: "About" },
+    { href: "#portfolio", content: "Work" },
+    { href: "#contact", content: "Contact" },
+  ];
+  const logoSrc = "./resources/logo.png";
+
+  const body = document.body;
+  const header = document.createElement("header");
+  header.classList.add("nav-bar");
+  // insert header at the top of body
+  body.prepend(header);
+
+  const nav = document.createElement("nav");
+  nav.classList.add("container", "nav-flex");
+
+  header.append(nav);
+
+  const a = document.createElement("a");
+  a.setAttribute("href", "#intro");
+
+  const logo = document.createElement("img");
+  logo.classList.add("logo");
+  logo.setAttribute("src", logoSrc);
+  logo.setAttribute("alt", "Logo");
+
+  a.append(logo);
+
+  nav.append(a);
+
+  const ul = document.createElement("ul");
+  ul.classList.add("nav-links");
+
+  links.forEach((link) => {
+    const li = document.createElement("li");
+    const aLink = document.createElement("a");
+    aLink.setAttribute("href", link.href);
+    aLink.textContent = link.content;
+
+    li.append(aLink);
+    ul.append(li);
+  });
+
+  nav.append(ul);
+
+  const hamburger = document.createElement("i");
+  hamburger.classList.add("burger", "fas", "fa-bars");
+
+  nav.append(hamburger);
+};
+
+const setupNav = () => {
   const burgerMenu = document.querySelector(".burger");
   const navLinks = document.querySelector(".nav-links");
   let opened = false;
@@ -11,8 +66,12 @@ const openNav = () => {
   burgerMenu.addEventListener("click", toggleNav);
   navLinks.addEventListener("click", toggleNav);
   window.addEventListener("resize", () => {
-    if (opened && document.documentElement.clientWidth >= 992) toggleNav();
+    if (opened && document.documentElement.clientWidth >= 992) {
+      toggleNav();
+    }
   });
 };
 
-openNav();
+// generate and setup navbar
+generateNav();
+setupNav();
